676c68440d191e00d5396fbeb72f6f4d6c3f9679
woulker <w.student.ces@gmail.com>
2017-04-21 18:56:54 -0500

    Update count.js

--- count.js
+++ count.js
@@ -1,7 +1,7 @@
 $(document).onload(getTime());
 function getTime() {
 	now = new Date();
-	fecha = new Date("2016-08-09 23:46:48 UTC");
+	fecha = new Date("2017-05-02 23:46:48 UTC");
 	days = (fecha - now) / 1000 / 60 / 60 / 24;
 	daysRound = Math.floor(days);
 	hours = (fecha - now) / 1000 / 60 / 60 - (24 * daysRound);
